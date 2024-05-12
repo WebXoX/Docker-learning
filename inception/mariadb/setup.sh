7e31ccf4f938d66a910d4ac91667b0f31b139dd5
Joe <97600250+WebXoX@users.noreply.github.com>
2024-05-12 17:36:27 +0000

    mariadb

--- inception/mariadb/setup.sh
+++ inception/mariadb/setup.sh
@@ -1,21 +1,47 @@
 #!/bin/sh
 
+echo "[i] mysqld not found, creating...."
 mkdir -p /run/mysqld
-chmod 777 /var/run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*skip-networking.*|skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-cat << EOF > /tmp/create_db.sql
+# if [ ! -d /var/lib/mysql/mysql ]; then
+echo "[i] MySQL data directory not found, creating initial DBs"
+chown -R mysql:mysql /var/lib/mysql
+mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+tfile=`mktemp`
+if [ ! -f "$tfile" ]; then
+	return 1
+fi
+# if [! -f]
+cat << EOF > $tfile
 USE mysql;
-FLUSH PRIVILEGES ;
+FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-CREATE DATABASE '$MYSQL_DATABASE';
-CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';
-FLUSH PRIVILEGES ;
+CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED by '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
 EOF
+/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+rm -f $tfile
+# fi
+    # only run if we have a starting MYSQL_DATABASE env variable AND
+    # the /docker-entrypoint-initdb.d/ file is not empty
+# execute any pre-exec scripts
+for i in /scripts/pre-exec.d/*sh
+do
+	if [ -e "${i}" ]; then
+		echo "[i] pre-exec.d - processing $i"
+		. ${i}
+	fi
+done
 
-/usr/bin/mysqld --user=mysql --bootstrap <  /tmp/create_db.sql
-	rm -f  /tmp/create_db.sql
+exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+# USE mysql;
+# FLUSH PRIVILEGES ;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+# CREATE DATABASE '$MYSQL_DATABASE';
+# GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';
+# CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+# GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';
+# FLUSH PRIVILEGES ;
