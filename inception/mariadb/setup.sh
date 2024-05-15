770045b884530ef32ca9f294ec08e202d397b3ea
Joe Shaju Perinchery <jperinch@lab3r3s11.42abudhabi.ae>
2024-05-15 16:02:07 +0400

    go

--- inception/mariadb/setup.sh
+++ inception/mariadb/setup.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 echo "[i] mysqld not found, creating...."
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+mkdir /var/run/mysqld
+chmod 777 /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+
+sed -i "s/#bind-address=0.0.0.0/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s/skip-networking/skip-networking=0/g" /etc/my.cnf.d/mariadb-server.cnf
+
 
 # if [ ! -d /var/lib/mysql/mysql ]; then
 echo "[i] MySQL data directory not found, creating initial DBs"
 chown -R mysql:mysql /var/lib/mysql
-mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+# mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
 tfile=`mktemp`
-if [ ! -f "$tfile" ]; then
-	return 1
-fi
-# if [! -f]
+# if ! [ -d "/var/lib/mysql/wordpress" ]; then
 cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
@@ -22,21 +26,11 @@ CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED by '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+/usr/bin/mysqld --user=mysql --bootstrap  < $tfile
 rm -f $tfile
 # fi
-    # only run if we have a starting MYSQL_DATABASE env variable AND
-    # the /docker-entrypoint-initdb.d/ file is not empty
-# execute any pre-exec scripts
-for i in /scripts/pre-exec.d/*sh
-do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-exec.d - processing $i"
-		. ${i}
-	fi
-done
 
-exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
+# exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
 # USE mysql;
 # FLUSH PRIVILEGES ;
 # ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
--- inception/wordpress/setup.sh
+++ inception/wordpress/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-cd /var/www/wordpress
+if [ ! -f ./wp-config.php ]; then
+    cp ./wp-config-sample.php ./wp-config.php
+    sed -i "s|database_name_here|${MYSQL_DATABASE}|g" ./wp-config.php
+    sed -i "s|username_here|${MYSQL_USER}|g" ./wp-config.php
+    sed -i "s|password_here|${MYSQL_PASSWORD}|g" ./wp-config.php
+    sed -i "s|localhost|mariadb|g" ./wp-config.php
+fi
+cd var/www/
 if ![  -f ./wp-config.php ]; then
-    wp core config --dbhost=${MYSQL_DATABASE}  --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
-    wp core install  --url="$DOMAIN_NAME" --title="$TITLE" --admin_user="$ADM_USER" --admin_password="$ADM_PASS" --admin_email="$ADM_EMAIL"
-    wp user create "$WP_USER" "$WP_USER_EMAIL" --role=subscriber --user_pass="$WP_USER_PASS" 
+    wp core config --dbhost=${MYSQL_HOST} --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
+    wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${ADM_USER} --admin_password=${ADM_PASS} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
+    wp user create ${WP_USER} ${WP_USER_EMAIL} --allow-root --role=subscriber --user_pass=${WP_USER_PASS}
 fi
