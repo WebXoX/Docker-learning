3d459bbc69c3aa4e0b941a5d874199c6dca7151e
Joe Shaju Perinchery <jperinch@lab3r8s1.42abudhabi.ae>
2024-05-16 09:23:23 +0400

    going to restart, mariadb is acting weird

--- inception/mariadb/setup.sh
+++ inception/mariadb/setup.sh
@@ -1,41 +1,56 @@
-#!/bin/sh
+# #!/bin/sh
 
-echo "[i] mysqld not found, creating...."
+# echo "[i] mysqld not found, creating...."
+# mkdir /var/run/mysqld
+# chmod 777 /var/run/mysqld
+# chown -R mysql:mysql /var/run/mysqld
+
+# sed -i "s/#bind-address=.*/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
+# sed -i "s/skip-networking.*/skip-networking=0/g" /etc/my.cnf.d/mariadb-server.cnf
+
+# echo "[i] MySQL data directory not found, creating initial DBs"
+# chown -R mysql:mysql /var/lib/mysql
+
+# mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+# tfile=`mktemp`
+# cat << EOF > $tfile
+# USE mysql;
+# FLUSH PRIVILEGES;
+# ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+# CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+# GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+# FLUSH PRIVILEGES;
+# EOF
+
+# /usr/bin/mysqld --user=mysql --bootstrap < $tfile
+# rm -f $tfile
+#!bin/sh
+
+chown -R mysql:mysql /var/lib/mysql
+
 mkdir /var/run/mysqld
 chmod 777 /var/run/mysqld
-chown -R mysql:mysql /var/run/mysqld
 
+# allow mariadb to accept remote conections and set mariadb to listen for any network interface
 sed -i "s/#bind-address=0.0.0.0/bind-address=0.0.0.0/g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s/skip-networking/skip-networking=0/g" /etc/my.cnf.d/mariadb-server.cnf
 
-
-# if [ ! -d /var/lib/mysql/mysql ]; then
-echo "[i] MySQL data directory not found, creating initial DBs"
-chown -R mysql:mysql /var/lib/mysql
-# mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
-
+# initialize the database
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
-tfile=`mktemp`
-# if ! [ -d "/var/lib/mysql/wordpress" ]; then
-cat << EOF > $tfile
+
+# create the .sql file to create the database
+cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED by '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-/usr/bin/mysqld --user=mysql --bootstrap  < $tfile
-rm -f $tfile
-# fi
 
-# exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
-# USE mysql;
-# FLUSH PRIVILEGES ;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-# CREATE DATABASE '$MYSQL_DATABASE';
-# GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';
-# CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-# GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE'.* TO '$MYSQL_USER'@'%';
-# FLUSH PRIVILEGES ;
+# run the file .sql to create the database
+/usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+rm -f /tmp/create_db.sql
--- inception/wordpress/setup.sh
+++ inception/wordpress/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd var/www/
 if [ ! -f ./wp-config.php ]; then
     cp ./wp-config-sample.php ./wp-config.php
     sed -i "s|database_name_here|${MYSQL_DATABASE}|g" ./wp-config.php
@@ -6,9 +7,8 @@ if [ ! -f ./wp-config.php ]; then
     sed -i "s|password_here|${MYSQL_PASSWORD}|g" ./wp-config.php
     sed -i "s|localhost|mariadb|g" ./wp-config.php
 fi
-cd var/www/
-if ![  -f ./wp-config.php ]; then
-    wp core config --dbhost=${MYSQL_HOST} --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
-    wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${ADM_USER} --admin_password=${ADM_PASS} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
-    wp user create ${WP_USER} ${WP_USER_EMAIL} --allow-root --role=subscriber --user_pass=${WP_USER_PASS}
-fi
+# if ![  -f ./wp-config.php ]; then
+#     wp core config --dbhost=${MYSQL_HOST} --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --allow-root
+    # wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${ADM_USER} --admin_password=${ADM_PASS} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
+    # wp user create ${WP_USER} ${WP_USER_EMAIL} --allow-root --role=subscriber --user_pass=${WP_USER_PASS}
+# fi
