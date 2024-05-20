af938ab211506ccfdd71d3a70f61873dc7473dc7
Joe <97600250+WebXoX@users.noreply.github.com>
2024-05-20 09:47:10 +0000

    fix

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -4,6 +4,7 @@ certbot --nginx -d ${DOMAIN} --email ${EMAIL} --agree-tos --non-interactive
 # openssl genpkey -algorithm RSA -out /etc/ssl/private/nginx.key -pkeyopt rsa_keygen_bits:2048
 
 # Generate the certificate
-openssl req -new -x509 -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/CN=${DOMAIN}/emailAddress=${EMAIL}"
+# openssl req -new -x509 -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/CN=${DOMAIN}/emailAddress=${EMAIL}"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt  -subj "/C=AE/ST=AbuDhabi/L=AbuDhabi/O=42AbuDhabi/CN="${DOMAIN}""
 
 # nginx -g daemon off;
