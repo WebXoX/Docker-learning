5f3b162a6410819aea9cfd16ed5e9b83b19a0128
joe <joeperinchery@gmail.com>
2024-05-20 21:55:11 +0400

    goo

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -16,7 +16,7 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
