7109219122496a57d22c5ad245b0fead494a5727
joe <joeperinchery@gmail.com>
2024-05-20 12:43:23 +0400

    ssl error

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -6,4 +6,4 @@ certbot --nginx -d ${DOMAIN} --email ${EMAIL} --agree-tos --non-interactive
 # Generate the certificate
 openssl req -new -x509 -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/OU=${ORGANIZATIONAL_UNIT}/CN=${DOMAIN}/emailAddress=${EMAIL}"
 
-nginx -g daemon off;
+# nginx -g daemon off;
